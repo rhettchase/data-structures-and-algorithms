92e42796318d6103a5d8b262d5051694b659d1e2
RHETT CHASE <rhettjchase@gmail.com>
2023-11-28 19:18:24 -0800

    passed test 7

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -96,7 +96,7 @@ For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
 const charCode = (arr) => {
-  // Solution code here...
+  return arr.map((element) => element.charCodeAt());
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -274,7 +274,7 @@ describe("Testing challenge 6", () => {
   });
 });
 
-xdescribe("Testing challenge 7", () => {
+describe("Testing challenge 7", () => {
   test("It should return an array containing the character code for each letter", () => {
     expect(charCode(["C", "o", "d", "e", "3", "0", "1"])).toStrictEqual([
       67, 111, 100, 101, 51, 48, 49,
