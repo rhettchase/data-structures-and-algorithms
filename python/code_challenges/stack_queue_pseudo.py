53d040569c90941e9b579f08533556ea289309f9
RHETT CHASE <rhettjchase@gmail.com>
2024-01-22 16:14:23 -1000

    passes all tests, updates readme excl whiteboard

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,37 @@
 from data_structures.stack import Stack
+from data_structures.invalid_operation_error import InvalidOperationError
 
 
 class PseudoQueue:
-    pass
+    """
+    Class uses 2 stacks to implement a queue's standard interface (the methods enqueue and dequeue).
+    """
+    def __init__(self):
+        """
+        Instantiate a new PseudoQueue that internally uses 2 stacks.
+        """
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def enqueue(self, value):
+        """
+        Inserts a value into the PseudoQueue, using a first-in, first-out approach.
+        """
+        self.stack1.push(value)
+
+    def dequeue(self):
+        """
+        Extracts a value from the PsuedoQueue, using a first-in, first-out approach.
+
+        Should raise exception when called on empty queue.
+        """
+        stack1 = self.stack1
+        stack2 = self.stack2
+        if stack2.is_empty():
+            if stack1.is_empty():
+                raise InvalidOperationError("Queue is empty")
+            while not stack1.is_empty():
+                top = stack1.pop()
+                stack2.push(top)
+        return stack2.pop()
+
